5d37a4e3b242b1b4cc20971273616df6260e8aac
Amabo Learn <97894724+amabolearn@users.noreply.github.com>
2023-04-20 17:06:30 +0100

    Add php for sidebar of lesson pages

---
+++ web-assets/scripts/php/aside-related-subject.php
@@ -0,0 +1,44 @@
+<html>
+ <head>
+ <style>
+  #related{
+   background-color: #FFFBEA;
+  }
+  #related a{
+   color: #7E6000;
+  }
+  .other-subject-wrapper ul{
+   list-style-type: none;
+   display: flex;
+  }
+ </style>
+ </head>
+ <body>
+<aside role="complementary" id="related">
+     <!--
+  <section id="related-links">
+   <h4>Related Topics</h4>
+   <ul class="w3-ul">
+    <li><a href="#">Related Link 1</a></li>
+      
+    <li><a href="#">Related Link 1</a></li>
+      
+    <li><a href="#">Related Link 2</a></li>
+      
+    <li><a href="#">Related Link 3</a></li>
+   </ul>
+  </section>-->
+     
+  <section class="other-subject-wrapper">
+   <h5>Other subjects</h5>
+   <ul>
+    <li><a href="http://amabolearn.atwebpages.com/mathematics/">Mathematics</a></li>
+      
+    <li><a href="http://amabolearn.atwebpages.com/biology/">Biology</a></li>
+      
+    <li><a href="http://amabolearn.atwebpages.com/physics/">Physics</a></li>
+   </ul>
+  </section>
+ </aside>
+ </body>
+</html>
