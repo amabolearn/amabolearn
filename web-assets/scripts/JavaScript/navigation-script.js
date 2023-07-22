3540aa336874eeedc29ce4aa2a96f3ef93726473
Amabo Learn <97894724+amabolearn@users.noreply.github.com>
2023-07-22 11:34:56 +0100

    Update navigation-script.js

--- web-assets/scripts/JavaScript/navigation-script.js
+++ web-assets/scripts/JavaScript/navigation-script.js
@@ -1,31 +1,16 @@
-let deviceWidth = window.innerWidth;
-console.log(deviceWidth)
-const menuBtn = document.querySelector('.menu-toggle');
-const activeElements = document.querySelectorAll('.active-element');
-const mainNav = document.getElementById("main-nav");
-let subjectLink = document.querySelector('.subject-link');
-const subNav = document.getElementById("sub-nav");
-const hamburger = document.querySelector(".menu-toggle i")
+function openMenu() {
+  document.getElementById('sideBAR').style.display = 'block';
+ } 
+ function closeMenu() {
+ document.getElementById('sideBAR').style.display = 'none'; 
+}
 
- for(i = 0; i < activeElements.length; i ++){
-  activeElements[i].classList.add('neutral-style');
-  subNav.classList.add('neutral-style');
- }
- menuBtn.addEventListener('click', () => {
-  for(i = 0; i < activeElements.length; i ++)
-   activeElements[i].classList.toggle('active');
-   hamburger.classList.toggle('fa-times')
-   
- });
- subjectLink.addEventListener('click', () => {
-  subNav.classList.toggle('active');
- })
- console.log(subjectLink)
-if(deviceWidth >= 1024){
- function prevDefAct(e){
-  e.preventDefault();
- }
- subjectLink.setAttribute("href", "#");
- subjectLink.addEventListener('click', prevDefAct);
- 
+function showSubj() {
+  var x = document.getElementById("subj-links");
+  
+  if (x.className.indexOf("w3-show") == -1) { 
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
 }
